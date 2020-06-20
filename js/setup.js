de1ad3f1e5fc3912d5178166988da0fbc47afd38
Karyakina Tatiana <tinka127@mail.ru>
2020-06-21 01:19:34 +0500

    add

--- js/setup.js
+++ js/setup.js
@@ -25,6 +25,12 @@ let COLOR_FIREBALL = [
     '#e6e848'
 ];
 
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SECOND_NAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_COUNT = 4;
+
 let setup = document.querySelector('.setup');
 let setupOpen = document.querySelector('.setup-open');
 let setupClose = setup.querySelector('.setup-close');
@@ -36,6 +42,53 @@ let valueCoat = document.querySelector('input[name="coat-color"]');
 let valueEyes = document.querySelector('input[name="eyes-color"]');
 let valueFireball = document.querySelector('input[name="fireball-color"]');
 
+function setWizardName() {
+    return getRandomValue(WIZARD_NAMES) + ' ' + getRandomValue(WIZARD_SECOND_NAME);
+}
+
+function getRandomValue(data) {
+    return data[Math.floor(Math.random() * data.length)];
+}
+
+function generateWizard() {
+    var wizard = {};
+    for (var i = 0; i < WIZARDS_COUNT; i++) {
+        wizard.name = setWizardName();
+        wizard.coatColor = getRandomValue(WIZARD_COAT_COLOR);
+        wizard.eyesColor = getRandomValue(WIZARD_EYES_COLOR);
+    }
+    return wizard;
+}
+
+function generateWizards() {
+    var wizards = [];
+    for (var i = 0; i < WIZARDS_COUNT; i++) {
+        wizards.push(generateWizard());
+    }
+    return wizards;
+}
+
+function getWizard(wizard) {
+    var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+        .content
+        .querySelector('.setup-similar-item');
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.coatColor;
+    return wizardElement;
+}
+
+function renderWizards() {
+    var similarListElement = document.querySelector('.setup-similar-list');
+    var fragment = document.createDocumentFragment();
+    var wizards = generateWizards();
+    wizards.forEach(function(wizard) {
+        fragment.appendChild(getWizard(wizard));
+    });
+    similarListElement.appendChild(fragment);
+}
+
 function rgb2hex(rgb) {
     var hexDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
     rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
@@ -44,10 +97,6 @@ function rgb2hex(rgb) {
         return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
     }
     return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-}
-
-function getRandomValue(data) {
-    return data[Math.floor(Math.random() * data.length)];
 }
 
 function onPopupEscPress(evt) {
@@ -63,7 +112,8 @@ function onPopupEscPress(evt) {
 
 function openPopup() {
     setup.classList.remove('hidden');
-
+    renderWizards();
+    setup.querySelector('.setup-similar').classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
 }
 
